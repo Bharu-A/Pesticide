ddabbbf21883276e826693a86ff79c356f48f4ee
Bharu-A <bharathkalmat108@gmail.com>
2025-10-31 22:40:19 +0530

    Updated DB connection for online host

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "root"; // Change to your MySQL username
-$password = ""; // Change to your MySQL password
-$dbname = "pesticide_locator";
+$servername = "sql12.freesqldatabase.com"; // your MySQL host
+$username = "sql12805516";                  // your MySQL username
+$password = "lQKYkc1wG6";                 // your MySQL password
+$dbname = "sql12805516";                    // your database name
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    http_response_code(500);
+    echo json_encode(["success" => false, "message" => "Database connection failed: " . $conn->connect_error]);
+    exit;
 }
 
-// Set charset to utf8
 $conn->set_charset("utf8");
 ?>
