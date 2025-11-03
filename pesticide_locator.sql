6ca833621f10dc4f12b5395d488a1ba536809570
Pravi24mr <praveenmr2004@gmail.com>
2025-11-04 00:21:04 +0530

    change 2

--- pesticide_locator.sql
+++ pesticide_locator.sql
@@ -23,33 +23,36 @@ CREATE TABLE `stores` (
   `address` varchar(255) NOT NULL,
   `phone_number` varchar(50) DEFAULT NULL,
   `map_link` text DEFAULT NULL,
+  `latitude` decimal(10,7) DEFAULT NULL,
+  `longitude` decimal(10,7) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `stores` (`name`, `address`, `phone_number`, `map_link`) VALUES
-('Sri ananth agro agencies', 'Jevargi - Chamarajanagar Rd, P.W.D. Camp, Sindhanur, Karnataka 584128', '099801 55505', 'https://share.google/w28PjgyFXOOHCYT5k'),
-('SRI SIDDIGANAPATI ENTERPRISES', 'Kunnatagi Camp, SINDHANUR, Karnataka 584128', '099726 84140', 'https://share.google/tRqkECmqgWKmywIsF'),
-('Jai Kisan Engineering', 'Opp. Dollars Colony, P.W.D. Camp, Sindhanur, Karnataka 584128', '098453 45233', 'https://share.google/RNInJnGD0UfmMWe5n'),
-('Sreedhar Agro Agencies', 'Shop no.55, New APMC Yard, Kushtagi Rd, Sindhanur, Karnataka 584128', '094481 23310', 'https://share.google/YF8P99S3CAJn0JcZe'),
-('Kartikeya Traders', '44, hedigibal camp, kolbal post, Sindhanur, SINDHANUR, Karnataka 584128', '7483509603', 'https://share.google/kizdGJQN17sxeFQDg'),
-('SRI DODDABASAVESHWARA TRADERS', 'QP8Q+92J, Kushtagi Rd, Sindhanur, Karnataka 584128', '085352 22677', 'https://share.google/KsaGEHv2CE9WpMW3g'),
-('SRI VASANTHM TRADERS', 'R.G. Road, JAWALAGERA-584143 Tq:Sindhanur, 584143', '8217369981', 'https://share.google/G95HRr7ueNCIcDTkC'),
-('DSR Corporation', 'QP9W+FX6, Sindhanur, Karnataka 584128', '085352 23388', 'https://share.google/vOlXBz7sZOo4kn2NS'),
-('Srishaila Mallekarjuna Agro Agences', 'Shop No 3, near by Nataraj Colony, Nataraj Colony, Raichur, Sindhanur, Karnataka 584128', '098808 73765', 'https://share.google/9vHBzfMULTuaR1fWF'),
-('Sri Raghavendra Agro Mart', 'Basavana Bhavi Chowk, 12-1-81/4, Gunj Rd, Jalal Nagar, Raichur, Karnataka 584102', '090084 90059', 'https://share.google/W2u9IkfDKgoYjpVKG'),
-('SAGAR ENTERPRISES', 'Basavana, Jodi Bhavi Road, Raichur, Karnataka 584102', NULL, 'https://share.google/uxq4nLxNg1btQ33RW'),
-('NAMMA GROMOR RAICHUR', 'Haji Colony, Raichur, Karnataka 584102', '097428 22792', 'https://share.google/IlBdUzIXVtep4ENF3'),
-('Sri Swamy Agro Agencies', 'Sri Swamy Agro Agencies, Basavan Bavi Circle, Karnataka 584102', '074062 82835', 'https://share.google/OyoWk7rYFr0BfThYd'),
-('Indian Agro Centre', 'Shop No: Poornima, Thaetre Complex, Gunj Main Rd, Jalal Nagar, Raichur, Karnataka 584102', '079754 60974', 'https://share.google/YkP2D3sQHPnX9szC4'),
-('VEENA ENTEPRISES', 'Rajendra Gunj, Raichur, Karnataka 584102', '094480 22655', 'https://share.google/8hyEZeRq2IRmqwzN1'),
-('Pruthvi Agro Seeds', '6965+RH9, Gunj Main Rd, Jalal Nagar, Basavanabhavi, Raichur, Karnataka 584102', '098864 52174', 'https://share.google/FpGkcM8V3VVQTmXBi'),
-('Agroes Services Pvt Ltd', 'Shop No.3, 9-20-59, Gadwal dharur road, Ganjipet Colony, Raichur, Karnataka 584102', '099000 92020', 'https://share.google/wgoDZ5isrEeVlvHLi'),
-('Sri Channabasaveshwara Agro Agency', 'Karadakal, main road, Lingsugur, Karnataka 584122', '097390 73989', 'https://share.google/8TadMRgdG5NBiGlKr'),
-('A One Agritech', 'Lingsugur, Karnataka 584122', '090086 67844', 'https://share.google/k15Y4RP1WmViefG4D'),
-('Raitamitra Agro Traders', '5G4C+HFG, Lingsugur, Karnataka 584122', NULL, 'https://share.google/as8JkuQttz5vh2WDq'),
-('SHREE SANGAMESHWARA KRUSHI KENDRA KALAPUR', 'Road, opposite Bus Stand, Lingsugur, Kalapur, Karnataka 584122', '099020 57390', 'https://share.google/3jrOa5jfX7OAL4kLy');
+INSERT INTO `stores` (`name`, `address`, `phone_number`, `map_link`, `latitude`, `longitude`) VALUES
+('Sri ananth agro agencies', 'Jevargi - Chamarajanagar Rd, P.W.D. Camp, Sindhanur, Karnataka 584128', '99801 55505', 'https://share.google/w28PjgyFXOOHCYT5k', 15.7830386, 76.7664126),
+('SRI SIDDIGANAPATI ENTERPRISES', 'Kunnatagi Camp, SINDHANUR, Karnataka 584128', '99726 84140', 'https://share.google/tRqkECmqgWKmywIsF', 15.8446673, 76.7244751),
+('Jai Kisan Engineering', 'Opp. Dollars Colony, P.W.D. Camp, Sindhanur, Karnataka 584128', '98453 45233', 'https://share.google/RNInJnGD0UfmMWe5n', 15.7912266, 76.7726224),
+('Sreedhar Agro Agencies', 'Shop no.55, New APMC Yard, Kushtagi Rd, Sindhanur, Karnataka 584128', '94481 23310', 'https://share.google/YF8P99S3CAJn0JcZe', 15.7664682, 76.7442776),
+('Kartikeya Traders', '44, hedigibal camp, kolbal post, Sindhanur, SINDHANUR, Karnataka 584128', '7483509603', 'https://share.google/kizdGJQN17sxeFQDg', 15.8704563, 76.6401345),
+('SRI DODDABASAVESHWARA TRADERS', 'QP8Q+92J, Kushtagi Rd, Sindhanur, Karnataka 584128', '85352 22677', 'https://share.google/KsaGEHv2CE9WpMW3g', 15.7659709, 76.7375117),
+('SRI VASANTHM TRADERS', 'R.G. Road, JAWALAGERA-584143 Tq:Sindhanur, 584143', '8217369981', 'https://share.google/G95HRr7ueNCIcDTkC', 15.8616858, 76.8143679),
+('DSR Corporation', 'QP9W+FX6, Sindhanur, Karnataka 584128', '85352 23388', 'https://share.google/vOlXBz7sZOo4kn2NS', 15.7686700, 76.7474043),
+('Srishaila Mallekarjuna Agro Agences', 'Shop No 3, near by Nataraj Colony, Nataraj Colony, Raichur, Sindhanur, Karnataka 584128', '98808 73765', 'https://share.google/9vHBzfMULTuaR1fWF', 15.7722363, 76.7519786),
+('Sri Raghavendra Agro Mart', 'Basavana Bhavi Chowk, 12-1-81/4, Gunj Rd, Jalal Nagar, Raichur, Karnataka 584102', '90084 90059', 'https://share.google/W2u9IkfDKgoYjpVKG', 16.2113390, 77.3588665),
+('SAGAR ENTERPRISES', 'Basavana, Jodi Bhavi Road, Raichur, Karnataka 584102', NULL, 'https://share.google/uxq4nLxNg1btQ33RW', 16.2116424, 77.3589037),
+('NAMMA GROMOR RAICHUR', 'Haji Colony, Raichur, Karnataka 584102', '97428 22792', 'https://share.google/IlBdUzIXVtep4ENF3', 16.2110342, 77.3508846),
+('Sri Swamy Agro Agencies', 'Sri Swamy Agro Agencies, Basavan Bavi Circle, Karnataka 584102', '74062 82835', 'https://share.google/OyoWk7rYFr0BfThYd', 16.2139787, 77.3592075),
+('Indian Agro Centre', 'Shop No: Poornima, Thaetre Complex, Gunj Main Rd, Jalal Nagar, Raichur, Karnataka 584102', '79754 60974', 'https://share.google/YkP2D3sQHPnX9szC4', 16.2124931, 77.3574339),
+('VEENA ENTEPRISES', 'Rajendra Gunj, Raichur, Karnataka 584102', '94480 22655', 'https://share.google/8hyEZeRq2IRmqwzN1', 16.2146819, 77.3591983),
+('Pruthvi Agro Seeds', '6965+RH9, Gunj Main Rd, Jalal Nagar, Basavanabhavi, Raichur, Karnataka 584102', '98864 52174', 'https://share.google/FpGkcM8V3VVQTmXBi', 16.2120488, 77.3589819),
+('Agroes Services Pvt Ltd', 'Shop No.3, 9-20-59, Gadwal dharur road, Ganjipet Colony, Raichur, Karnataka 584102', '99000 92020', 'https://share.google/wgoDZ5isrEeVlvHLi', 16.2097276, 77.3617717),
+('Sri Channabasaveshwara Agro Agency', 'Karadakal, main road, Lingsugur, Karnataka 584122', '97390 73989', 'https://share.google/8TadMRgdG5NBiGlKr', 16.1608345, 76.5192491),
+('A One Agritech', 'Lingsugur, Karnataka 584122', '90086 67844', 'https://share.google/k15Y4RP1WmViefG4D', 16.1576029, 76.5212247),
+('Raitamitra Agro Traders', '5G4C+HFG, Lingsugur, Karnataka 584122', NULL, 'https://share.google/as8JkuQttz5vh2WDq', 16.1564481, 76.5212053),
+('SHREE SANGAMESHWARA KRUSHI KENDRA KALAPUR', 'Road, opposite Bus Stand, Lingsugur, Kalapur, Karnataka 584122', '99020 57390', 'https://share.google/3jrOa5jfX7OAL4kLy', 16.1958782, 76.5124815);
+
 COMMIT;
-COMMIT;
+
 
 
 INSERT INTO `pesticides` (`id`, `name`, `crop`, `description`, `price`, `category`) VALUES
